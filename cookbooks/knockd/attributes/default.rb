5e335ec6062960e7f029c6fff88b9fb6aa7efaf1
Aldo Borrero <aldo@aldoborrero.com>
2012-12-11 18:39:59 +0100

    Converted cookbooks dir to submodules.

--- cookbooks/knockd/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default['knockd']['rules']['ssh'] = {
-  'sequence_size' => 3,
-  'port' => 22,
-  'timeout' => 10
-}
--- cookbooks/knockd/libraries/default.rb
+++
@@ -1,5 +0,0 @@
-def generate_sequence(length)
-  result = []
-  length.to_i.times { result << 10000 + (rand * 10000).to_i }
-  result
-end
